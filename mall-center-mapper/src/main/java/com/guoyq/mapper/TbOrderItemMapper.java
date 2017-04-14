16970270f2a237c9ddc40c3cbc808fcc6b43d817
guoyiqing1 <guoyiqing1@jd.com>
2017-04-14 15:29:32 +0800

    add user

---
+++ mall-center-mapper/src/main/java/com/guoyq/mapper/TbOrderItemMapper.java
@@ -0,0 +1,31 @@
+package com.guoyq.mapper;
+
+import com.guoyq.pojo.TbOrderItem;
+import com.guoyq.pojo.TbOrderItemExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TbOrderItemMapper {
+    int countByExample(TbOrderItemExample example);
+
+    int deleteByExample(TbOrderItemExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(TbOrderItem record);
+
+    int insertSelective(TbOrderItem record);
+
+    List<TbOrderItem> selectByExample(TbOrderItemExample example);
+
+    TbOrderItem selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") TbOrderItem record, @Param("example") TbOrderItemExample example);
+
+    int updateByExample(@Param("record") TbOrderItem record, @Param("example") TbOrderItemExample example);
+
+    int updateByPrimaryKeySelective(TbOrderItem record);
+
+    int updateByPrimaryKey(TbOrderItem record);
+}
