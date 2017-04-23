f6df1d9380257f5b37b765c206374ee71b769c4e
guoyiqing1 <guoyiqing1@jd.com>
2017-04-23 11:21:54 +0800

    add ftp相关

---
+++ mall-center-web/src/main/java/com/guoyq/controller/PictureController.java
@@ -0,0 +1,22 @@
+package com.guoyq.controller;
+
+import com.guoyq.service.PictureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+public class PictureController {
+
+    @Autowired
+    private PictureService pictureService;
+
+    @RequestMapping("/pic/upload")
+    @ResponseBody
+    public Map pictureUpload(MultipartFile uploadFile){
+        Map resultMap=pictureService.uploadPicture(uploadFile);
+        return resultMap;
+    }
+}
